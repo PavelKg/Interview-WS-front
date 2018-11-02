7aa8eb4b2f83f5e825b64efb40b39ab7bb98cebb
pavel.pavlenko <pavel.pavlenko@it-atlantic.com>
2018-11-02 13:37:23 +0600

    prepare login page

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,6 @@ export default {
 }
 </script>
 
-<style>
-#app {}
+<style lang="scss">
+@import "./assets/styles";
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,46 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+// const ifNotAuthenticated = (to, from, next) => {
+//   if (!store.getters.isAuthenticated) {
+//     next()
+//     return
+//   }
+//   next('/')
+// }
+
+// const ifAuthenticated = (to, from, next) => {
+//   if (store.getters.isAuthenticated) {
+//     next()
+//     return
+//   }
+//   next('/login')
+// }
+
+const Home = {template: '<h1>Home</h1>'}
+const Interview = {template: '<h1>Interview</h1>'}
+const Login = {template: '<h1>Login</h1>'}
+// const ErrRoute = {template: '<h1>404</h1>'}
+
 export default new Router({
   mode: 'history',
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/interview',
+      name: 'Interview',
+      component: Interview
+      // beforeEnter: ifAuthenticated,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+      // beforeEnter: ifNotAuthenticated,
+    }
+  ]
 })

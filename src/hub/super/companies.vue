fc00893853fc978f03a21194ab9aaa0b3aea69a0
pavel.pavlenko <pavel.pavlenko@it-atlantic.com>
2019-01-05 15:24:33 +0600

    add page function2

--- src/hub/super/companies.vue
+++ src/hub/super/companies.vue
@@ -17,4 +17,44 @@
     </tr>
     </thead>
   </table>  
-</template>        
+</template>  
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  export default {
+    name: "companies",
+    computed: {
+      ...mapGetters([
+        'companies',
+      ]),
+    }
+  }
+</script> 
+
+<style lang="scss">
+  .tb-companies {
+    width: 100%;
+    max-width: 100%;
+    margin-bottom: 16px;
+    margin-bottom: 1rem;
+    background-color: transparent;
+    border-collapse: collapse;
+    th {
+      padding: 15px 0;
+      border-bottom: 2px solid #dee2e6;
+    }
+    td {
+      padding: 12px;
+      padding: .75rem;
+      vertical-align: middle; 
+      border-top: 1px solid #dee2e6;
+    }
+    .cell-icon {
+      width: 5%;
+    }
+    .gray {
+        background-color: #f2f2f2;
+    }
+  }
+</style>
--- src/hub/super/content.vue
+++ src/hub/super/content.vue
@@ -1,17 +1,18 @@
 <template>
   <div class='content'>
+    <Companies/>
   </div>  
 </template>
 
 <script>
   import { mapGetters } from 'vuex'
+  import Companies from './companies'
 
   export default {
     name: "content-area",
-    computed: {
-      ...mapGetters([
-        'companies',
-      ]),
+    computed: {},
+    components: {
+      Companies
     }
   }
 </script> 
@@ -20,29 +21,5 @@
   .content {
     padding: 20px 20px;
     width: 100%;
-    .tb-companies {
-      width: 100%;
-      max-width: 100%;
-      margin-bottom: 16px;
-      margin-bottom: 1rem;
-      background-color: transparent;
-      border-collapse: collapse;
-      th {
-        padding: 15px 0;
-        border-bottom: 2px solid #dee2e6;
-      }
-      td {
-        padding: 12px;
-        padding: .75rem;
-        vertical-align: middle; 
-        border-top: 1px solid #dee2e6;
-      }
-      .cell-icon {
-        width: 5%;
-      }
-      .gray {
-          background-color: #f2f2f2;
-      }
-    }
   }
 </style>

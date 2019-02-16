ed0cb433243aaf3580beda219a657c1e7e731bf3
pavel.pavlenko <pavel.pavlenko@it-atlantic.com>
2019-02-16 12:32:49 +0600

    impr page videos

--- src/hub/super/videos/player.vue
+++ src/hub/super/videos/player.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>
-    <span>Player</span>
-    <video ref="videoPlayer" controls autoplay ></video>
-  </div>
+  <div class="browse-video-area">
+    <div class='player-zone'>
+      <video ref="videoPlayer" controls></video>
+      <div class="player-zone-company-info">
+        <p>ID:001 ABCD</p>
+        <p>https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8</p>
+      </div>
+    </div>
+    <div class='player-link-zone'>
+      <span>Back to the company page</span>
+      <span>Back to the HOME</span>
+    </div>
+  </div>  
 </template>
+
 <script>
 import Hls from 'hls.js'
 
@@ -19,7 +29,7 @@ export default {
         video.play();
     })
   } else {
-      addSourceToVideo(video, 'https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8', 'video/mp4');
+      addSourceToVideo(video, 'https://p0.oc.kg:8081/video/c/hd3/Mortal_Engines_HD.mp4', 'video/mp4');
       video.play();
     }
 
@@ -33,6 +43,28 @@ export default {
 }
 </script>
 <style lang="scss">
-
+  .browse-video-area {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    .player-zone {
+      display: flex;
+      flex-direction: row;
+      padding: 20px;
+      video {
+        max-width: 870px;
+        max-height: 500px;
+      }
+      .player-zone-company-info {
+        padding: 0 40px ;
+      }
+    }
+    .player-link-zone {
+      padding: 20px;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;      
+      width: 50%;
+    }
+  }  
 </style>
->
